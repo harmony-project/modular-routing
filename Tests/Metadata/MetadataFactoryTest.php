b30ef93311f827b82e69fb18a8dc655baef0355f
Tim Goudriaan <tim@codedmonkey.com>
2016-05-08 19:21:42 +0200

    Move MetadataFactoryTest to a new namespace

--- Tests/Metadata/MetadataFactoryTest.php
+++ Tests/Metadata/MetadataFactoryTest.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Harmony\Component\ModularRouting\Tests;
+namespace Harmony\Component\ModularRouting\Tests\Metadata;
 
 use Harmony\Component\ModularRouting\Metadata\MetadataFactory;
 
