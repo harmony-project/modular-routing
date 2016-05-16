f547b0cad365b880d1fc404d74d1f2f349868890
Tim Goudriaan <tim@codedmonkey.com>
2016-05-17 01:03:49 +0200

    Create ModularTrait trait for entities that can be mapped to a module

---
+++ Model/ModularTrait.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Harmony\Component\ModularRouting\Model;
+
+/**
+ * ModularTrait
+ *
+ * To be embedded in entities that are mapped to a module.
+ *
+ * @author Tim Goudriaan <tim@harmony-project.io>
+ */
+trait ModularTrait
+{
+    /**
+     * @var ModuleInterface
+     */
+    protected $module;
+
+    /**
+     * @param ModuleInterface|null $module
+     */
+    public function setModule(ModuleInterface $module = null)
+    {
+        $this->module = $module;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModule()
+    {
+        return $this->module;
+    }
+}
