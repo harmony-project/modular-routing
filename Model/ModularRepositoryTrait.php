575e45d48e6c77483b0c2290e357e360f4efeb31
Tim Goudriaan <tim@codedmonkey.com>
2016-11-22 11:52:22 +0100

    Added ModularRepositoryTrait for classes that need to check whether a Module instance is valid to remove the reliance on the Module instance if the ModularRouting component is not implemented.

---
+++ Model/ModularRepositoryTrait.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Harmony\Component\ModularRouting\Model;
+
+/**
+ * ModularRepositoryTrait
+ *
+ * Adds useful repository methods for modular entities.
+ */
+trait ModularRepositoryTrait
+{
+    /**
+     * Checks if a Module instance is valid.
+     *
+     * By implementing this method, repositories can simply check if the Module instance is invalid
+     * to determine that it doesn't have an association with the given Module. Enabling the use of
+     * the entity with StaticModule instances or without implementing the ModularRouting component.
+     *
+     * @param ModuleInterface|null $module
+     *
+     * @return bool Returns false if the instance is invalid or not modular
+     */
+    protected function isModular(ModuleInterface $module = null)
+    {
+        if (null == $module) {
+            return false;
+        }
+
+        if ($module instanceof StaticModule) {
+            return false;
+        }
+
+        return true;
+    }
+}
